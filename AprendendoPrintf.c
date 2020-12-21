33319a9a9c90c58735b1b852097216f793a03877
Matheus Henrique Orsini <matheus1506200@outlook.com>
2020-12-20 22:37:00 -0300

    Aprendendo Printf

---
+++ AprendendoPrintf.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void main()
+{
+    /* Para mostrar variáveis no printf:
+        printf ("%tipo da variável", nome da variável);
+        int = %i ou %d
+        float / double = %f
+        char = %c
+        string = %s
+        módulo = %u
+        endereço de memória = %p
+        notação científica = %E ou %e
+        imprimir porcentagem = %%
+
+        Variáveis com casas decimais imprimem até as casas não sendo utilizada, portanto, deve-se especificar quantas casas serão utilizadas.
+            printf("%.numero de casas + tipo", nome);
+    */
+    int i = 15;
+    char c = 'A';
+    char string [18] = "Abertura de porta";
+    float f = 10.52;
+    double d = 15.98364;
+    printf("%i\n", i);
+    printf("%f\n", f);
+    printf("%s\n", string);
+    printf("%.3f\n", d); //Note que foi arredondado a 3 casa para um valor acima, pois o número subsequente era >= 5.
+    printf("%c\n", c);
+    printf("%i\n%.2f\n%.4f\n%c\n", i, f, d, c); // Imprimindo varias variaveis ao mesmo tempo.
+}
