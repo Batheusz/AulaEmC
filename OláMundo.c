1ac54789fc737859c807e882d6283cf9208b93d3
Matheus Henrique Orsini <matheus1506200@outlook.com>
2020-12-20 21:52:46 -0300

    Update OláMundo.c

--- OláMundo.c
+++ OláMundo.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-void main(){
-
-    printf("Ola Mundo!\n");
-    
+void main()
+{
+    printf("Olá Mundo!\n");
 }
+//Comentar uma linha
+/*Comentar várias linhas
+variável tipo char = "A", "2", ""
+variável tipo int = números inteiros 32mil +-
+variável tipo float = números quebrados 7 casas
+variável tipo void = vazio
+variável tipo double = número com 15 casas decimais
+*/
