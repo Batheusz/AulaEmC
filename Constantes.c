64791646fcc2d8b1d57949e773ca6d61d1b5168b
Matheus Henrique Orsini <matheus1506200@outlook.com>
2020-12-23 15:39:37 -0300

    Create Constantes.c

---
+++ Constantes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//Podemos definir constantes ou por código ou por comando, ex:
+// const Nome = Valor;
+// #define nome valor
+
+void main()
+{
+    // calcular área de uma circunferência.
+    const double Pi = 3.14159265;
+    float Raio;
+    printf("Digite seu raio:\n>>>");
+    scanf("%f", &Raio);
+    printf("Sua area eh: %f metros quadrados", Pi * pow(Raio,2));
+
+}
