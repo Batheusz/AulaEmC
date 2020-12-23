d9a7a0945907b27ab7c3bd13581e32c442a05699
Matheus Henrique Orsini <matheus1506200@outlook.com>
2020-12-23 15:22:30 -0300

    Create Break_Continue.c

---
+++ Break_Continue.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    // Break -> Interrompe o laço
+    // Continue -> Interrompe o ciclo
+    for( int x = 0; x <= 100; x++)
+    {
+        //Quando executado saimos do ciclo e continuamos o for.
+        if(x == 20)
+        {
+            continue;
+        }
+        //Quando executado saimos do laço for.
+        if (x == 70)
+        {
+            break;
+        }
+        printf("%i ", x);
+    }
+}
